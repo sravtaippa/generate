0a5dfa0f1ab547ccb63050b8190668d99bc83178
sravtaippa <sravan@taippa.com>
2025-02-13 08:52:59 +0400

    Testing Integrated data sync

--- app/pipelines/icp_generation.py
+++ app/pipelines/icp_generation.py
@@ -96,7 +96,8 @@ def generate_icp(client_id,website_url):
                 "icp_locations":str(person_locations),
                 "page_number":'1',
                 "qualify_leads":'no',
-                "records_required":'5'
+                "records_required":'5',
+                "is_active":"yes",
             }
             export_to_airtable(config_data, CLIENT_CONFIG_TABLE_NAME)
         return dynamic_url
