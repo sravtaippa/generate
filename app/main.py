544b2447d69259f3d3b59da1648800de2045f14c
Shibla-tp <shiblashilu@taippa.com>
2025-05-14 14:07:41 +0400

    leads email updation

--- app/main.py
+++ app/main.py
@@ -26,7 +26,8 @@ from dashboard.client_onboarding_update_form import update_client_onboarding
 from dashboard.client_configuration_form import update_client_configuration
 from dashboard.client_profile_picture import get_profile_picture
 from dashboard.recent_leads_gmail import fetch_recent_leads_from_db, fetch_metric_value, get_booking_count, email_sent_chart, get_campaign_details
-from pipelines.organization_list_enrichment import fetch_organization_domains
+from dashboard.recent_leads_gmail import fetch_airtable_data_and_create_csv
+from dashboard.leads_email import fetch_organization_domains
 # from pipelines.guideline_generate import generate_content_guideline
 from pipelines.data_sanitization_psql import sanitize_data
 from pipelines.guideline_generate import execute_generate_sequence
@@ -524,9 +525,13 @@ def get_booking_count_dashboard():
 def get_email_sent_chart_dashboard(username):
     return email_sent_chart(username)
 
-@app.route("/get_campaign_details_dashboard/<username>", methods=["GET"])
-def get_campaign_details_dashboard(username):
-    return get_campaign_details(username)
+@app.route("/fetch_airtable_data_and_create_csv", methods=["GET"])
+def fetch_airtable_data_and_create_csv():
+    return fetch_airtable_data_and_create_csv()
+
+@app.route("/get_campaign_details_dashboard", methods=["GET"])
+def get_campaign_details_dashboard():
+    return get_campaign_details()
 
 @app.route("/test_db",methods=["GET"])
 def connect_db():
