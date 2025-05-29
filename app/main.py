ea4591459bb7b7c8449551dda0b8149298c041f5
Shibla-tp <shiblashilu@taippa.com>
2025-05-29 16:06:02 +0400

    test database

--- app/main.py
+++ app/main.py
@@ -809,27 +809,27 @@ def run_email_post_response_tracker():
 @app.route("/run_booking_meeting_tracker", methods=["GET"])
 def run_booking_meeting_tracker():
     try:
-        # Text_Content = request.args.get("Text_Content", default=None)
-        # data = {
-        #     "Text_Content": Text_Content
-        # }
+        Text_Content = request.args.get("Text_Content", default=None)
         data = {
-            "Text_Content": """
-                Hi Urmi C,
-
-                A new Schedule an Appointment with SHIBLA THADATHIL PARAMBIL has been scheduled for 2025-02-28 at 15:30 Europe/Paris
-
-                Event Details
-
-                Email: shiblashilusaif@gmail.com
-                Phone: N/A
-                Country: N/A
-                Location: https://meet.google.com/yap-tuuv-mba
-                Password: N/A
-                Event description:
-                """
-
+            "Text_Content": Text_Content
         }
+        # data = {
+        #     "Text_Content": """
+        #         Hi Urmi C,
+
+        #         A new Schedule an Appointment with SHIBLA THADATHIL PARAMBIL has been scheduled for 2025-02-28 at 15:30 Europe/Paris
+
+        #         Event Details
+
+        #         Email: shiblashilusaif@gmail.com
+        #         Phone: N/A
+        #         Country: N/A
+        #         Location: https://meet.google.com/yap-tuuv-mba
+        #         Password: N/A
+        #         Event description:
+        #         """
+
+        # }
         booking_meeting_tracker(data)
         return {"status": "success", "message": "Testing completed"}
     except Exception as e:
