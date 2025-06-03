13afa9af6f824000f793cd5af86f235e409b6f66
sravtaippa <sravan@taippa.com>
2025-06-03 18:51:11 +0530

    updated leadsin API call

--- app/outreach/add_leads.py
+++ app/outreach/add_leads.py
@@ -26,7 +26,15 @@ def add_lead_leadsin(apollo_id,campaign_id,outreach_table_name):
         last_name = record.get("recipient_last_name","Not available")
         message1 = record.get("linkedin_message","Not available")
         message2 = record.get("linkedin_message_2","Not available")
+        message3 = record.get("linkedin_message_3","Not available")
+        message4 = record.get("linkedin_message_4","Not available")
+        message5 = record.get("linkedin_message_5","Not available")
         subject1 = record.get("linkedin_subject","Not available")
+        subject2 = record.get("linkedin_subject_2","Not available")
+        subject3 = record.get("linkedin_subject_3","Not available")
+        subject4 = record.get("linkedin_subject_4","Not available")
+        subject5 = record.get("linkedin_subject_5","Not available")
+
         connection_message = record.get("linkedin_connection_message","Not available")
 
         print(f"Profile URL: {profileUrl}")
@@ -36,7 +44,14 @@ def add_lead_leadsin(apollo_id,campaign_id,outreach_table_name):
         print(f"Connection Message: {connection_message}")
         print(f"Message 1: {message1}")
         print(f"Message 2: {message2}")
+        print(f"Message 3: {message3}")
+        print(f"Message 4: {message4}")
+        print(f"Message 5: {message5}")
         print(f"Subject 1: {subject1}")
+        print(f"Subject 2: {subject2}")
+        print(f"Subject 3: {subject3}")
+        print(f"Subject 4: {subject4}")
+        print(f"Subject 5: {subject5}")
 
         profile_details = {
         "apollo_id":apollo_id,
@@ -47,7 +62,14 @@ def add_lead_leadsin(apollo_id,campaign_id,outreach_table_name):
         "connection_message": connection_message,
         "message1": message1,
         "message2": message2,
+        "message3": message3,
+        "message4": message4,
+        "message5": message5,
         "subject1": subject1,
+        "subject2": subject2,
+        "subject3": subject3,
+        "subject4": subject4,
+        "subject5": subject5,
         "removeDbDuplicates":"true",
         }
         
