b0cdafc221718f7c36416a09c9edfd04f054f7bd
Shibla-tp <shiblashilu@taippa.com>
2025-06-09 13:05:44 +0400

    test database

--- app/pipelines/data_extractor.py
+++ app/pipelines/data_extractor.py
@@ -67,7 +67,7 @@ def people_search_v2(search_url,client_id,qualify_leads,index_name):
     print(f"\n---------- Started Persona Data Mining for client : {client_id} ----------")
     print(f"Search url: {search_url}")
     print(f"Headers: {APOLLO_HEADERS}")
-    response = requests.get(search_url, headers=APOLLO_HEADERS)
+    response = requests.post(search_url, headers=APOLLO_HEADERS)
     print(f"Execution status code: {response.status_code}")
     if response.status_code == 200:
         print(f"\n ---------- Completed Persona Data Mining for client : {client_id} ----------")
