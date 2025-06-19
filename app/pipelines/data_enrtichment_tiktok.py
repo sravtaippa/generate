bd037b529b0d5dac9cf43c103fe2072757d005e1
Shibla-tp <shiblashilu@taippa.com>
2025-06-19 18:17:49 +0400

    tiktok updation

--- app/pipelines/data_enrtichment_tiktok.py
+++ app/pipelines/data_enrtichment_tiktok.py
@@ -4,7 +4,7 @@ import requests
 import json
 import traceback
 from config import AIRTABLE_API_KEY, AIRTABLE_BASE_ID, APIFY_API_TOKEN
-AIRTABLE_TABLE_NAME='src_influencer_data'
+AIRTABLE_TABLE_NAME='src_influencer_data_demo'
 # Airtable setup
 api = Api(AIRTABLE_API_KEY)
 airtable = api.table(AIRTABLE_BASE_ID, AIRTABLE_TABLE_NAME)
