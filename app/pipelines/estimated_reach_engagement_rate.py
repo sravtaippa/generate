d35c6e0b4db599dbb8cfa01e2be9594a7d519a52
Shibla-tp <shiblashilu@taippa.com>
2025-06-27 14:11:29 +0400

    tiktok updation

--- app/pipelines/estimated_reach_engagement_rate.py
+++ app/pipelines/estimated_reach_engagement_rate.py
@@ -2,24 +2,37 @@ from flask import Flask, request, jsonify
 from db.db_ops import db_manager
 app = Flask(__name__)
 
-def calculate_metrics(followers, likes, comments, reach_rate=0.35):
+def calculate_metrics(username, reach_rate):
     try:
-        followers = int(followers)
+        table_name = "src_influencer_data_demo"
+        cols_list = ["tiktok_username"]
+        col_values = [username]
+        result = db_manager.get_records_with_filter(table_name, cols_list, col_values, limit=1)
+        if result:
+            return {"status": "failed", "message": f"No record found for {username}"}
+        
+        record = result if isinstance(result, dict) else result[0]
+        print(f"✅ Record: {record}", flush=True)
 
+        followers = record.get("followers", "")
+        comments = record.get("comments", "")
+        likes = record.get("likes", "")
+        followers_count = int(followers)
+            
         if not isinstance(likes, list) or not isinstance(comments, list):
             return {"status": "failed", "error": "Likes and comments must be lists"}, 400
 
-        likes = [int(l) for l in likes]
-        comments = [int(c) for c in comments]
+        likes_count = [int(l) for l in likes]
+        comments_count = [int(c) for c in comments]
 
-        avg_likes = sum(likes) / len(likes) if likes else 0
-        avg_comments = sum(comments) / len(comments) if comments else 0
+        avg_likes = sum(likes_count) / len(likes_count) if likes_count else 0
+        avg_comments = sum(comments_count) / len(comments_count) if comments_count else 0
 
-        if followers <= 0:
+        if followers_count <= 0:
             return {"status": "failed", "error": "Invalid follower count"}, 400
 
-        engagement_rate = ((avg_likes + avg_comments) / followers) * 100
-        estimated_reach = followers * reach_rate
+        engagement_rate = ((avg_likes + avg_comments) / followers_count) * 100
+        estimated_reach = followers_count * reach_rate
         data = {
             "engagement_rate": engagement_rate,
             "estimated_reach": estimated_reach
@@ -27,8 +40,8 @@ def calculate_metrics(followers, likes, comments, reach_rate=0.35):
         db_manager.insert_data("src_influencer_data_demo", data)
         return {
             "status": "success",
-            "total_likes": likes,
-            "total_comments": comments,
+            "total_likes": likes_count,
+            "total_comments": comments_count,
             "engagement_rate": round(engagement_rate, 2),
             "estimated_reach": int(estimated_reach)
         }, 200
